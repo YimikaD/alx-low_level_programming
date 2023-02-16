7a5a3c1329b5a1f81c11e755fb0bd012ff01bb3e
YimikaD <babsyinny@gmail.com>
2023-02-16 09:57:22 -0800

    4-puts.c commit

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that prints a line using the put function
+ * Return: value of 0
+ */
+int main(void)
+{
+puts("\"Programming is like building a multilingual puzzle");
+return (0);
+}
