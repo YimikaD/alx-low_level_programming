7ae0f6772275efc24a6681c4bdcf3baba7d275ec
YimikaD <babsyinny@gmail.com>
2023-02-16 10:17:57 -0800

    5-printf.c commit

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,10 @@
+#include <stdio.h>
+/**
+ * main - C program that print a line using the prinf function
+ * Return: 0 value
+ */
+int main(void)
+{
+printf("with proper grammar, but the outcome is a piece of art,\n");
+return (0);
+}
