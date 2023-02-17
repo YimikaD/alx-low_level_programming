641412eb471d608d7fd59749a958d552e3698717
YimikaD <babsyinny@gmail.com>
2023-02-16 17:31:25 -0800

    commit

--- 0x00-hello_world/101-quote.c
+++ 0x00-hello_world/101-quote.c
@@ -1,12 +1,11 @@
 #include <stdio.h>
-#include <unixstd.h>
+#include <unistd.h>
  /**
- * main - Entry point
- * Description: C program that prints a line to the standard error
+ * main - C program that prints a line to the standard error
  * Return: 1 (Success)
  */
 int main(void)
 {
-write(2, "and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", 58);
+fput("and that piece of art is useful\" - Dora Korpar, 2015-10-19\n", stdout);
 return (1);
 }
