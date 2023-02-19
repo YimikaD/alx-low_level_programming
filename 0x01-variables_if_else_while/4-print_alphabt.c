c814195facf52e910d8412a77684619b4226ffa8
YimikaD <babsyinny@gmail.com>
2023-02-19 14:01:33 -0800

    4-print_alphabt.c commit

---
+++ 0x01-variables_if_else_while/4-print_alphabt.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Program that prints alphabets except q qnd e in lowercase
+ * Return:0 (Success)
+ */
+int main(void)
+{
+	char i;
+	for (i = 'a'; i <= 'z'; i++)
+	{
+		if (i != 'q' && i != 'e')
+			putchar(i);
+	}
+	putchar('\n')
+	return (0);
+}
+
