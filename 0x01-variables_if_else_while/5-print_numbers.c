0f3b92ade1dd828ef2c68a0b0460d01d38ffaa18
YimikaD <babsyinny@gmail.com>
2023-02-19 14:24:26 -0800

    5-print_numbers.c commit

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,14 @@
+#include <stdio.h>
+/**
+ * main - program that prints all single digits nums starting from 0 of base 10
+ * Return: 0 (Success)
+ */
+int main (void)
+{
+	int n;
+
+	for (n = 0; n < 10; n++)
+		printf("%d", n);
+	printf('\n');
+	return (0);
+}
