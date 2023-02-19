c817170b152f8b99abcaac8394817b9675430e7f
YimikaD <babsyinny@gmail.com>
2023-02-19 06:07:21 -0800

    2-print_alphabet.c commit

--- 0x01-variables_if_else_while/2-print_alphabet.c
+++ 0x01-variables_if_else_while/2-print_alphabet.c
@@ -12,6 +12,6 @@ int main(void)
 	{
 		putchar(alp[i]);
 	}
-	putchar('\n');
+	putchar("abcdefghijklmnopqrstuvwxyz");
 	return (0);
 }
--- 0x01-variables_if_else_while/3-print_alphabets.c
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -10,7 +10,7 @@ int main(void)
 	char i;
 
 	for (i = 'A'; i <= 'Z'; i++);
-	putchar("%C", i)
+	putchar("%C", i);
 		for (i = 'a'; i <= 'z'; i++);
 	putchar("%c", i);
 	return (0);
