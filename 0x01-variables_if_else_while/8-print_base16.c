726f50cc46a723fb74ba6fb1394572dbdf04780a
YimikaD <babsyinny@gmail.com>
2023-02-20 08:31:40 -0800

    8-print_base16.c commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -1,18 +1,17 @@
 #include <stdio.h>
 /**
- * main - Program that prints digits of number of base 16 in lowercase
- * Return: 0 (Success)
- */
+* main - Program that prints digits of number of base 16 in lowercase
+* Return: 0 (Success)
+*/
 int main(void)
 {
 	int nums;
 	char la;
 	
-	for (nums = 0; nums < 10; nums++)
+for (nums = 0; nums < 10; nums++)
 		putchar((nums % 10) + '0');
-	for (la = 'a'; la <= 'f'; la++);
+for (la = 'a'; la <= 'f'; la++);
 	putchar(la);
-
-	putchar('\n');
-	return (0);
+putchar('\n');
+return (0);
 }
