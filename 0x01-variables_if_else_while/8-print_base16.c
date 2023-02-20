eacafc8e9df4ac256d95408393ad9e1d52bc5208
YimikaD <babsyinny@gmail.com>
2023-02-20 08:33:21 -0800

    8-print_base16.c commit

--- 0x01-variables_if_else_while/8-print_base16.c
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -10,7 +10,7 @@ int main(void)
 	
 for (nums = 0; nums < 10; nums++)
 		putchar((nums % 10) + '0');
-for (la = 'a'; la <= 'f'; la++);
+for (la = 'a'; la <= 'f'; la++)
 	putchar(la);
 putchar('\n');
 return (0);
