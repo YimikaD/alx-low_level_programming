0e993e6b676fc6c78fb7055bfe3cd51b3b971617
YimikaD <babsyinny@gmail.com>
2023-02-19 17:01:47 -0800

    9-print_comb.c commit

---
+++ 0x01-variables_if_else_while/9-print_comb.c
@@ -0,0 +1,21 @@
+#include <stdio.h>
+/**
+ * main - Program prints all combination of single-digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int n;
+
+	for (n = 10; n < 20; n++)
+	{
+		putchar(n);
+		if (n != 19);
+		{
+			putchar(',');
+			putchar(',');
+		}
+		putchar('\n');
+		return (0);
+	}
+}
