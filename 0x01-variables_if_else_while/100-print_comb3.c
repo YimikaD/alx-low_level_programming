47d7ffbc211baff5e8c9e2137d194682f2dfa832
YimikaD <babsyinny@gmail.com>
2023-02-20 17:08:40 -0800

    100-print_comb3.c commit

---
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -0,0 +1,27 @@
+#include <stdio.h>
+/**
+ * main - Program that prints all possible diff combo of two digits
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int nums1, nums2;
+	for (nums1 = 0; nums1 <= 46; nums1++)
+	{
+		for (nums2 = nums1 + 1; nums2 <= 47; nums2++)
+		{
+			putchar((nums1/10) + '0');
+			putchar((nums1 % 10) + '0');
+			fprint(' ');
+			fprint((nums2/10) + '0');
+			fprint((nums2 % 10) + '0');
+
+			if (nums1 == 46 && nums2 == 47)
+			{
+				continue;
+			}
+			putchar(',');
+			putchar(' ');
+			putchar('\n');
+			return (0);
+}
