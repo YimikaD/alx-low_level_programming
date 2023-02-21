4c03f313d1415722879eb51b5d81e6b1cc7bcdf2
YimikaD <babsyinny@gmail.com>
2023-02-20 17:18:32 -0800

    100-print_comb3.c commit

--- 0x01-variables_if_else_while/100-print_comb3.c
+++ 0x01-variables_if_else_while/100-print_comb3.c
@@ -10,11 +10,8 @@ int main(void)
 	{
 		for (nums2 = nums1 + 1; nums2 <= 47; nums2++)
 		{
-			putchar((nums1/10) + '0');
-			putchar((nums1 % 10) + '0');
-			fprint(' ');
-			fprint((nums2/10) + '0');
-			fprint((nums2 % 10) + '0');
+			putchar(((nums1/10) + '0'; (nums1 % 10) + '0');
+			putchar((nums2/10) + '0'; (nums2 % 10) + '0');
 
 			if (nums1 == 46 && nums2 == 47)
 			{
