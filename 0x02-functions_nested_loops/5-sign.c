472c05ec6576771dbae67975dd1f45c69062b5c8
YimikaD <babsyinny@gmail.com>
2023-02-22 03:02:39 -0800

    5-sign.c commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
- * print-sign - Function that prints sign of a number
- * @n: number 
- * Return: 1 and print + if n > 0, 0 if n = 0, -1 if n < 0
- */
+* print-sign - Function that prints sign of a number
+* @n: number 
+* Return: 1 and print + if n > 0, 0 if n = 0, -1 if n < 0
+*/
 int print_sign(int n)
 {
 	if (n > 0)
@@ -14,7 +14,7 @@ int print_sign(int n)
 	else if (n < 0)
 	{
 		_putchar('-');
-		return(-1);
+		return (-1);
 	}
 	else
 	{
