79f2c5cbc54c36074da4bba537d0e1f8ace67bf9
YimikaD <babsyinny@gmail.com>
2023-02-21 16:52:08 -0800

    2-prints_alphabetx10 commit

---
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * print_alphabetx10 - prints 10times in lowercase
+ * Return: void
+ */
+void print_alphabet_x10(void)
+{
+	char c;
+	int i = 0;
+
+	while (i <= 0)
+	{
+		for (c = 'a'; c <= 'z'; c++)
+		{
+			_putchar(c);
+		}
+			_putchar('\n');
+			i++;
+	}
+}
