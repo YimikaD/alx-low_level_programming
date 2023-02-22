eaf78d925aa2d67f46ccf2bee138c458b6ad3fdf
YimikaD <babsyinny@gmail.com>
2023-02-22 15:41:52 -0800

    103-fibonacci.c commit

---
+++ 0x02-functions_nested_loops/103-fibonacci.c
@@ -0,0 +1,11 @@
+#include <stdio.h>
+#include <stdlib.h>
+#include "main.h"
+/**
+ * main - Program that prints sum of even-value terms
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	return (0);
+}
