3753d5b87207e734549153765d83efcd88f9e5c0
YimikaD <babsyinny@gmail.com>
2023-02-22 05:41:40 -0800

    8-24_hours.c commit

--- 0x02-functions_nested_loops/8-24_hours.c
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -5,19 +5,18 @@
  */
 void jack_bauer(void)
 {
-	int a, b;
+int a, b;
 
-	 for (a = 0; a < 24; a++)
-	{
-		for (b = 0; b < 60; b++)
-		{
-			_putchar((a / 10) + '0');
-			_putchar((a % 10) + '0');
-			_putchar(':');
-			_putchar((b / 10) + '0');
-			_putchar((b % 10) + '0');
-			_putchar('\n');
-		}
-	}
+for (a = 0; a < 24; a++)
+{
+for (b = 0; b < 60; b++)
+{
+_putchar((a / 10) + '0');
+_putchar((a % 10) + '0');
+_putchar(':');
+_putchar((b / 10) + '0');
+_putchar((b % 10) + '0');
+_putchar('\n');
 }
-
+}
+}
