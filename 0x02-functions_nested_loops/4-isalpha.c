8cee221a3ab7a551482d549dd57e0494ce6b1509
YimikaD <babsyinny@gmail.com>
2023-02-21 17:28:26 -0800

    4isalpha commit

---
+++ 0x02-functions_nested_loops/4-isalpha.c
@@ -0,0 +1,13 @@
+#include "main.h"
+/**
+ * -isalpha.c - checks for alphabetic character
+ *  @c: characterbto be checked
+ *  Return: 1 if c is a lower or uppercase letter, 0 if otherwise
+ */
+int _isalpha(int c)
+{
+	if (((c >= 'a') && (c <= 'z')) || ((c >= 'A') && (C <= 'Z')))
+		return (1);
+	else
+		return (0);
+}
