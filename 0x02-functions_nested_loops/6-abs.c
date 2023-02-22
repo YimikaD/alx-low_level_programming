740c6407f958b1bbe0c77f6894103f1c551b2524
YimikaD <babsyinny@gmail.com>
2023-02-22 03:18:39 -0800

    6-abs.c commi

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * 6-abs.c - Function that computes the absolute value of an integer
+ * Return: Always 0
+ */
+int _abs(int)
+{
+	if (c < 0)
+	{
+		int abs_val;
+
+		abs_val(c * -1);
+		return (abs_val);
+	}
+	return (c);
+}
