d7aca5036b5b64a34aa768127e23ed7c07c7d81e
YimikaD <babsyinny@gmail.com>
2023-02-22 12:47:48 -0800

    commit

--- 0x02-functions_nested_loops/5-sign.c
+++ 0x02-functions_nested_loops/5-sign.c
@@ -2,7 +2,7 @@
 /**
 * print_sign - Function that prints sign of a number
 * @n: number 
-* Return: 1 and print + if n > 0, 0 if n = 0, -1 if n < 0
+* Return: 1 and print+ if n > 0, 0 if n = 0, -1 if n < 0
 */
 int print_sign(int n)
 {
