0478a9d895fe1c90a686810f0c5e3fc3f4c4022f
YimikaD <babsyinny@gmail.com>
2023-02-22 10:38:27 -0800

    100-times_table.c commit

---
+++ 0x02-functions_nested_loops/100-times_table.c
@@ -0,0 +1,10 @@
+#include "main.h"
+/**
+ * print_times_table - Function that prints n times-table, starting with 0
+ * @n: number input
+ * Return: 0 (Success)
+ */
+void print_times_table(int n)
+{
+	return (0);
+}
