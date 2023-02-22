02a75e622e809383c29bbc5e7c348f1410cde7d1
YimikaD <babsyinny@gmail.com>
2023-02-22 05:24:17 -0800

    8-24_hours.c commit

---
+++ 0x02-functions_nested_loops/8-24_hours.c
@@ -0,0 +1,23 @@
+#include "main.h"
+/**
+ * 8-24_hours.c - Prints every mins of the day of Jack Bauer from 00:00 to 23:59
+ * Return: 24hr clock 
+ */
+void jack_bauer(void)
+{
+	int a, b;
+
+	 for (a = 0; a < 24; a++)
+	{
+		for (b = 0; b < 60; b++)
+		{
+			_putchar((a / 10) + '0');
+			_putchar((a % 10) + '0');
+			_putchar(':');
+			_putchar((b / 10) + '0');
+			_putchar((b % 10) + '0');
+			_putchar('\n');
+		}
+	}
+}
+
