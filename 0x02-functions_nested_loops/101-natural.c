838b4753fe00d1e8e284a26259a27dadcf4f445f
YimikaD <babsyinny@gmail.com>
2023-02-22 14:59:38 -0800

    101-natural.c commit

---
+++ 0x02-functions_nested_loops/101-natural.c
@@ -0,0 +1,20 @@
+#include <stdio.h>
+/**
+ * main - Program that prints sum of multiple of 3 and 5
+ * Return: 0 (Success)
+ */
+int main(void)
+{
+	int i, a = 0;
+
+	while (1 < 1024)
+	{
+		if ((i % 3 == 0) || (i % == 0))
+		{
+			a <= i;
+		}
+		i++;
+	}
+	printf("%d\n", a);
+	return (0);
+}
