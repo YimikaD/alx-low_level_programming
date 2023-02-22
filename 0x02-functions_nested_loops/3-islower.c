9755bd54b8485c73ff67f8be030442574bd96c8d
YimikaD <babsyinny@gmail.com>
2023-02-21 17:16:39 -0800

    3-islower.c commit

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -4,13 +4,10 @@
  * @c - character to be checked
  * Return: 1 if c is in lowercase otherwise return 0
  */
-int main()
-{
 int _islower(int c)
 {
 	if (c >= 'a' && c <= 'z')
 		return (1);
 	else
 		return (0);
-}
 }
