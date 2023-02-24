2e23f3fcbffc84cf167801837c46cdb989e19daa
YimikaD <babsyinny@gmail.com>
2023-02-23 16:24:23 -0800

    2-mul.c commit

---
+++ 0x04-more_functions_nested_loops/2-mul.c
@@ -0,0 +1,11 @@
+#include "main.h"
+/**
+ * mul - Function that multilplies two integers
+ * @a: First integer
+ * @b: Second integer
+ * Return: (a * b)
+ */
+int mul(int a, int b)
+{
+	return (a * b);
+}
