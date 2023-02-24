73abd5653dffbaa22b4252d207fa5a4f35702a0d
YimikaD <babsyinny@gmail.com>
2023-02-24 11:55:01 -0800

    7-print commit

---
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -0,0 +1,27 @@
+#include "main.h"
+/**
+ * print_diagonal - draws a diagonal line
+ * @n: number of lines to print diagonal line
+ * Return: void
+ */
+void print_diagonal(int n)
+{
+	int x, y;
+
+	if (n <= 0)
+	{
+		_putchar('\n');
+	}
+	else
+	{
+		for (x = 0; x < n; X++)
+		{
+			for (y = 0; y < n; y++)
+			{
+				_putchar(32);
+			}
+			_putchar(92);
+			_putchar('\n');
+		}
+	}
+}
