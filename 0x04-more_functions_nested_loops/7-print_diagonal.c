c156537619222863f7a7d7470fdb1792c0aa2f8a
YimikaD <babsyinny@gmail.com>
2023-02-24 12:13:07 -0800

    7-print_diagonal.c commit

--- 0x04-more_functions_nested_loops/7-print_diagonal.c
+++ 0x04-more_functions_nested_loops/7-print_diagonal.c
@@ -16,12 +16,12 @@ void print_diagonal(int n)
 	{
 		for (x = 0; x < n; x++)
 		{
-			for (y = 0; y < n; y++)
-			{
-				_putchar(32);
-			}
-			_putchar(92);
-			_putchar('\n');
+		for (y = 0; y < n; y++)
+		{
+		_putchar(32);
+		}
+		_putchar(92);
+		_putchar('\n');
 		}
 	}
 }
