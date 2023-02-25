c30e30b2dabca116e7ed2ea59725f7bcd490c1ac
YimikaD <babsyinny@gmail.com>
2023-02-24 18:32:05 -0800

    3-print commit

--- 0x04-more_functions_nested_loops/3-print_numbers.c
+++ 0x04-more_functions_nested_loops/3-print_numbers.c
@@ -5,11 +5,12 @@
  */
 void print_numbers(void)
 {
-	int a;
+char c = '0';
 
-	for (a = 0; a <= 9; a++)
-	{
-	_putchar(a + '0');
-	}
-	_putchar('\n');
+while (c <= '9')
+{
+_putchar(c);
+c++;
+}
+_putchar('\n');
 }
