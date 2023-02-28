2e5cbee36691b74651401eb6fbc5705d6fba31eb
YimikaD <babsyinny@gmail.com>
2023-02-27 20:23:26 -0800

    101 commit

--- 0x05-pointers_arrays_strings/101-keygen.c
+++ 0x05-pointers_arrays_strings/101-keygen.c
@@ -27,7 +27,7 @@ if ((sum - 2722) % 2 != 0)
 diff_half1++;
 for (index = 0; password[index]; index++)
 {
-if (password[index] <= (33 + diff_half1))
+if (password[index] >= (33 + diff_half1))
 {
 password[index] -= diff_half1;
 break;
@@ -36,7 +36,7 @@ break;
 }
 for (index = 0; password[index]; index++)
 {
-if (password[index] <= (33 + diff_half2))
+if (password[index] >= (33 + diff_half2))
 {
 password[index] -= diff_half2;
 break;
