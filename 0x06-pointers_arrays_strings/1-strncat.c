0154d534d2d7128491169a6793b88e0dc87d9f43
YimikaD <babsyinny@gmail.com>
2023-03-02 05:56:29 -0800

    1-strncat.c commit

---
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -0,0 +1,20 @@
+#include "main.h"
+/**
+ * *_strncat - concatenates two strings
+ * @src: source string
+ * @dest: destination string
+ * @n: number of bytes to be append to dest
+ * Return: a pointer dest
+ */
+char *_strncat(char *dest, char *src, int n)
+{
+	int index = 0, dest_len = 0;
+
+	while (dest[index++])
+	{
+		dest_len++;
+	}
+	for (index = 0; src[index] && index < n; index++)
+		dest_len[dest_len++] = src[index];
+	return (dest);
+}
