cf4a90649dbf2424dce3952cdf8f0b0b04cba0f0
YimikaD <babsyinny@gmail.com>
2023-03-02 15:28:45 -0800

    5-string_toupper.c commit

---
+++ 0x06-pointers_arrays_strings/5-string_toupper.c
@@ -0,0 +1,18 @@
+#include "main.h"
+/**
+ * *string_toupper - function that changes all lowercase string to uppercase
+ * @str: string to be changed
+ * Return: pointer to the changed string
+ */
+char *string_toupper(char *str)
+{
+	int index = 0;
+
+	while (str[index])
+	{
+		if (str[index] >= 'a' && str[index] <= 'z')
+			str[index] -= 32;
+		index++;
+	}
+	return (str);
+}
