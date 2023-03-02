15ede806b6038cf5a3ed0f73db349a6118a8617e
YimikaD <babsyinny@gmail.com>
2023-03-02 14:51:19 -0800

    -rev_array.c commit

---
+++ 0x06-pointers_arrays_strings/4-rev_array.c
@@ -0,0 +1,17 @@
+#include "main.h"
+/**
+ * reverse_array - function that reverse the content of an  array of integers
+ * @a: an array of integers
+ * @n: number of elements to swap
+ * Return: void
+ */
+void reverse_array(int *a, int n);
+{
+	int tmp, index;
+
+	for (index = n - 1; index >= n / 2; index--)
+	{
+		tmp = a[n - 1 - index];
+		a[n - 1 - index] = a[index];
+		a[index] = tmp;
+	}
