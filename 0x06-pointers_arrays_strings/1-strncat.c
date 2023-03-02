5d2a665ded8ee05d74921349f3eaf732c8cae323
YimikaD <babsyinny@gmail.com>
2023-03-02 06:02:46 -0800

    1-strncat.c commit

--- 0x06-pointers_arrays_strings/1-strncat.c
+++ 0x06-pointers_arrays_strings/1-strncat.c
@@ -16,7 +16,7 @@ char *_strncat(char *dest, char *src, int n)
 	}
 	for (index = 0; src[index] && index < n; index++)
 	{
-		dest_len[dest_len++] = src[index];
+		dest[dest_len++] = src[index];
 	}
 	return (dest);
 }
