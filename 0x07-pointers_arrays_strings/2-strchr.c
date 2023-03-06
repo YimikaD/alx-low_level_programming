7aeb067e204008cd7d7869fde4f675c0ffefa982
YimikaD <babsyinny@gmail.com>
2023-03-06 15:38:13 -0800

    2-strchr.ccommit

--- 0x07-pointers_arrays_strings/2-strchr.c
+++ 0x07-pointers_arrays_strings/2-strchr.c
@@ -1,9 +1,9 @@
 #include "main.h"
 /**
  * *_strchr - function that locates a character in a string
- * @s: string 
+ * @s: string
  * @c: the character
- * Return: a pointer to the first occurrence of the character @c in the string
+ * Return: a pointer to the first occurrence @c in the string
  * @s, or NULL if the character is not found
  */
 char *_strchr(char *s, char c)
@@ -17,4 +17,3 @@ char *_strchr(char *s, char c)
 	}
 	return (NULL);
 }
-
