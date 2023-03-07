d73d43c69ce982d57cebff77dc7302fb3fdbd7da
YimikaD <babsyinny@gmail.com>
2023-03-06 16:37:14 -0800

    3-strspn commit

--- 0x07-pointers_arrays_strings/3-strspn.c
+++ 0x07-pointers_arrays_strings/3-strspn.c
@@ -1,6 +1,6 @@
 #include "main.h"
 /**
- * int _strspn - function that gets the length of a prefix substring
+ * _strspn - function that gets the length of a prefix substring
  * @s: initial segments
  * @accept: bytes included
  * Return: number of @s which consist only @accept
