fcf3dd58e4d744eb8d700d2af10e391787d4bc82
YimikaD <babsyinny@gmail.com>
2023-03-07 11:19:24 -0800

    5-sqrt_recursion.c commit -m

--- 0x08-recursion/5-sqrt_recursion.c
+++ 0x08-recursion/5-sqrt_recursion.c
@@ -1,11 +1,11 @@
 #include "main.h"
 /**
- * _sqrt_recursion - returns natural square root of a number
+ * _sqrt - returns natural square root of a number
  * @n: number to calculate the number
  * @i: ileterate number
  * Return: square root
  */
-int _sqrt_recursion(int n, int i)
+int _sqrt(int n, int i)
 {
 	int sqrt = i * i;
 
