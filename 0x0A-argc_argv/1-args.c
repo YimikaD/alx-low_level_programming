39c0112b5e4a9af51f4053c80b769ad8d2d396c5
YimikaD <babsyinny@gmail.com>
2023-03-10 11:49:00 -0800

    1-args.c commit

---
+++ 0x0A-argc_argv/1-args.c
@@ -0,0 +1,14 @@
+#include "main.h"
+#include <stdio.h>
+/**
+ * main - Program that prints number of arguments passed into it
+ * @argc: argunment count
+ * @argv: argument vector
+ * Return: 0 (Success)
+ */
+int main(int argc, char *argv[])
+{
+	(void) argv;
+	printf("%d\n", argc-1);
+	return (0);
+}
