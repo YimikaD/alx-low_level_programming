5a051e27965c565b8ea17b10f9232d7aced23675
YimikaD <babsyinny@gmail.com>
2023-03-14 12:50:25 -0700

    3-alloc_grid.c commit

--- 0x0B-malloc_free/3-alloc_grid.c
+++ 0x0B-malloc_free/3-alloc_grid.c
@@ -9,28 +9,28 @@
 int **alloc_grid(int width, int height)
 {
 	int a, b, c, d;
-	char **arr;
+	char **ar;
 
 	if (width <= 0 || height <= 0)
 	{
 		return (NULL);
 	}
-	arr = malloc(sizeof(int *) * height);
+	ar = malloc(sizeof(int *) * height);
 	if (arr == NULL)
 	{
-		free(arr);
+		free(ar);
 		return (NULL);
 	}
 	for (a = 0; a < height; a++)
 	{
-		arr[a] = malloc(sizeof(int) * width);
-	if (arr[a] == NULL)
+		ar[a] = malloc(sizeof(int) * width);
+	if (ar[a] == NULL)
 	{
 		for (b = a; b >= 0; b--)
 		{
 			free(arr[b]);
 		}
-		free(arr);
+		free(ar);
 		return (NULL);
 	}
 }
@@ -38,8 +38,8 @@ for (c = 0; c < height; c++)
 {
 	for (d = 0; d < width; d++)
 	{
-		arr[c][d] = 0;
+		ar[c][d] = 0;
 	}
 }
-return (arr);
+return (ar);
 }
