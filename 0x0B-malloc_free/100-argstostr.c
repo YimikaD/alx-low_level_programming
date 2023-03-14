d8d42b4e3650225ce67bc0ee53dcdbaedc476990
YimikaD <babsyinny@gmail.com>
2023-03-14 16:27:28 -0700

    vim 100-argstostr.c commmit

---
+++ 0x0B-malloc_free/100-argstostr.c
@@ -0,0 +1,45 @@
+#include "main.h"
+#include <stdio.h>
+#include <stdlib.h>
+/**
+ * *argstostr - concatenates all the programs arguments
+ * @argc: argument count
+ * @argv: argument vector
+ * Return: 0
+ */
+char *argstostr(int ac, char **av)
+{
+	int a = 0, b = 0, c = 0, d = 0;
+	char *s;
+
+	if (ac == 0 || av == NULL)
+		return (NULL);
+	while (b < ac)
+	{
+		while (av[b][c])
+		{
+			a++;
+			b++;
+		}
+		c = 0;
+		b++;
+	}
+	s = malloc((sizeof(char) * a) +  ac + 1);
+	b = 0;
+	while (av[b])
+	{
+		while (av[b][c])
+				{
+				s[d] = av[b][c];
+				d++;
+				b++;
+				}
+				s[d] = '\n';
+				b = 0;
+				d++;
+				b++;
+				}
+				d++;
+				s[d] = '\0';
+				return (s);
+				}
