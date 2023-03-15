69c1c21022dc26be385c63a1ff6ee1ba2ad5170a
YimikaD <babsyinny@gmail.com>
2023-03-14 17:39:16 -0700

    101-strtow.c commit

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,6 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 #include <stdlib.h>
+#include <string.h>
 /**
  * **strtow - split string into word
  * @str: string to split
@@ -8,30 +9,40 @@
  */
 char **strtow(char *str)
 {
-	if (str == NULL || str == "")
+	if (str == NULL || strlen(str) == "")
 		return (NULL);
 }
-// Count the number of words in the string
+/**
+ * Count the number of words in the string
+ */
     int num_words = 0;
-    char *pch = strtok(str, " ");
+    char *pch = strtoq(str, " ");
+
     while (pch != NULL) {
         num_words++;
         pch = strtok(NULL, " ");
     }
     
-    // Allocate memory for the array of words
+    /**
+     * Allocate memory for the array of words
+     */
     char **words = (char **) malloc((num_words+1) * sizeof(char *));
     if (words == NULL) {
         return NULL;
     }
     
-    // Split the string into words and store them in the array
+    /**
+     * Split the string into words and store them in the array
+     */
     pch = strtok(str, " ");
     for (int i = 0; i < num_words; i++) {
         int word_len = strlen(pch);
         words[i] = (char *) malloc((word_len+1) * sizeof(char));
         if (words[i] == NULL) {
-            // If malloc fails, free any previously allocated memory and return NULL
+            /**
+	     * If malloc fails, free any previously allocated
+	     * memory and return NULL
+	     */
             for (int j = 0; j < i; j++) {
                 free(words[j]);
             }
@@ -41,7 +52,6 @@ char **strtow(char *str)
         strcpy(words[i], pch);
         pch = strtok(NULL, " ");
     }
-    words[num_words] = NULL; // Set the last element of the array to NULL
-    
+    words[num_words] = NULL;
     return words;
 }
