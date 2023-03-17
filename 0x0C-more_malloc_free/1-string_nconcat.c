c2ae3c08501c96e538ac8dff3470d9a3c36d5bde
YimikaD <babsyinny@gmail.com>
2023-03-16 17:30:40 -0700

    1-string_nconcat.c commit

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -10,7 +10,7 @@
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
 {
-	unsigned int 1 = 0, j = 0, k = 0, l = 0;
+	unsigned int i = 0, j = 0, k = 0, l = 0;
 	char *ptr;
 
 	if (s1 == NULL)
