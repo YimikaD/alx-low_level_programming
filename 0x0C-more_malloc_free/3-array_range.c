45f1fb25047cd4349926ad9c8e980834cffb4e96
YimikaD <babsyinny@gmail.com>
2023-03-16 18:10:46 -0700

    3-array_range commit

--- 0x0C-more_malloc_free/3-array_range.c
+++ 0x0C-more_malloc_free/3-array_range.c
@@ -13,13 +13,14 @@ int *array_range(int min, int max)
 
 	if (min > max)
 		return (NULL);
-	arr = malloc(sizeof(int) * (max - min) + sizeof(int));
+	arr = malloc((sizeof(int) * (max - min)) + sizeof(int));
 	if (arr == NULL)
 		return (NULL);
 	while (min <= max)
 	{
 		arr[i] = min;
 		i++;
+		min++;
 	}
 	return (arr);
 }
