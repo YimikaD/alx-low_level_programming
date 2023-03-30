df99f2a6c99e4093a917fd321953cf574ab20cc0
YimikaD <babsyinny@gmail.com>
2023-03-30 09:51:12 -0700

    Function that prints

---
+++ 0x12-singly_linked_lists/100-first.c
@@ -0,0 +1,15 @@
+#include <stdio.h>
+
+void print(void) __linked__ ((statement));
+
+/**
+  * print - function to print
+  *
+  * Return: Nothing.
+  */
+void print(void)
+{
+	printf("You're beat! and yet, you must allow,\n");
+	printf("I bore my house upon my back!\n");
+}
+
